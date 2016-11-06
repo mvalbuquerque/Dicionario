9c32507f08d61e70c2ced6a4705e2da4115d5906
mvalbuquerque <albuquerquenet@icloud.com>
2016-11-06 10:58:21 -0200

    Dicionario

---
+++ Dicionarios.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str1 = "Meses do Ano"
+
+var meses = [Int: String]()
+
+meses[1]  = "January"
+meses[2]  = "February"
+meses[3]  = "March"
+meses[4]  = "April"
+meses[5]  = "May"
+meses[6]  = "June"
+meses[7]  = "July"
+meses[8]  = "Agoust"
+meses[9]  = "September"
+meses[10] = "October"
+meses[11] = "November"
+meses[12] = "December"
+
+print(meses[1]!)
+print("------------------")
+
+var str2 = "Marcas"
+
+var marcas = [String: String]()
+
+marcas["VW"]     = "Fusca"
+marcas["GM"]     = "Onix"
+marcas["BMW"] = "X1"
+marcas["Audi"]   = "A1"
+
+
+print(marcas["VW"]!)
